70150911a752e6e91597bb9ccfca54a3bb6eec60
swatsam29 <ssampathkumar@uco.edu>
2022-06-06 20:06:53 -0500

    Drawer

--- lib/viewscreen/start_screen.dart
+++ lib/viewscreen/start_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:lesson1/viewscreen/view/view_util.dart';
 
@@ -26,7 +27,49 @@ class StartScreen extends StatelessWidget {
               icon: const Icon(Icons.message)),
         ],
       ),
-      body: const Text('Start Screen'),
+      drawer: Drawer(
+        child: ListView(
+          children:  [
+            const UserAccountsDrawerHeader(
+              currentAccountPicture: Icon(Icons.account_circle_rounded, size: 80.0 ,),
+              accountName: Text('ssampathkumar'), 
+              accountEmail: Text('ssampathkumar@uco.edu'),
+              ),
+              ListTile(
+                leading: const Icon(Icons.people),
+                title: const Text('Friends'),
+                onTap: (){
+                  Navigator.of(context).pop(); //close the drawer
+                  showSnackBar(context: context, message: 'Friends menu');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.exit_to_app),
+                title: const Text('Logout'),
+                onTap: (){
+                  Navigator.of(context).pop(); //close the drawer
+                  showSnackBar(context: context, message: 'Logout menu');
+                },
+              ),
+
+          ],
+          ),
+        ),
+      body: Column(
+        children: [
+          const Text('Choose a menu to navigate'),
+          ElevatedButton(
+            onPressed: () =>
+                showSnackBar(context: context, message: 'Button 1'),
+            child: const Text('Button 1'),
+          ),
+          ElevatedButton(
+            onPressed: () =>
+                showSnackBar(context: context, message: 'Button 2'),
+            child: const Text('Button 2'),
+          ),
+        ],
+      ),
     );
   }
 }
